e33facab3c32d1882e23ae8dc1b113174806d7d3
webxo <ekunkoya@gmail.com>
2019-02-05 19:05:54 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
   #used to connect to the databse
-  $host = "localhost";
-  $db_name = "leavedb";
-  $username = "root";
-  $password = "";
+  $host = "uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+  $db_name = "b5c8mr62u90k1r87";
+  $username = "lov2vb2cbeotxkhl";
+  $password = "dkafbkunz5zwztu3";
 
   try {
       $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
